00887ecea6cf932b8b88d3f09e829a7755e20096
Samyou0228 <samyukthak3333@gmail.com>
2026-04-21 15:22:33 +0530

    d1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
